4bfa251752e8e49bcf8d97b7c4d3b091bcab4a34
Airi-tech <airis1631@gmail.com>
2020-07-17 11:05:55 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,10 @@
 class MessagesController < ApplicationController
   def index
   end
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-end
->>>>>>> parent of 1f191a8... Add:　メッセージ送信機能を実装
+
   def create
   end
-<<<<<<< HEAD
 
   private
 
@@ -23,18 +16,3 @@ end
     @group = Group.find(params[:group_id])
   end
 end
-=======
-end
->>>>>>> parent of a676ac5... Add:　該当するアクションをコントローラに定義
-=======
->>>>>>> parent of 1f191a8... Add:　メッセージ送信機能を実装
-=======
-end
-  def create
-  end
->>>>>>> parent of 1f191a8... Add:　メッセージ送信機能を実装
-=======
-end
-  def create
-  end
->>>>>>> parent of 1f191a8... Add:　メッセージ送信機能を実装
