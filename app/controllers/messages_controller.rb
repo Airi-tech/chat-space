c61845a3e4a86c6aa06cb9f626b5391c0f557770
Airi-tech <airis1631@gmail.com>
2020-07-17 11:03:31 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
   end
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 end
@@ -25,4 +26,9 @@ end
 end
 >>>>>>> parent of a676ac5... Add:　該当するアクションをコントローラに定義
 =======
+>>>>>>> parent of 1f191a8... Add:　メッセージ送信機能を実装
+=======
+end
+  def create
+  end
 >>>>>>> parent of 1f191a8... Add:　メッセージ送信機能を実装
