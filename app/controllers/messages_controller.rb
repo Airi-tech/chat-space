e07566cfd65ba14f2b9ffd10cfc47db1541fd8a2
Airi-tech <airis1631@gmail.com>
2020-07-17 11:00:37 +0900

    Revert　該当するアクションをコントローラに定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,7 @@ class MessagesController < ApplicationController
     @message = Message.new
     @messages = @group.messages.includes(:user)
   end
+<<<<<<< HEAD
 
   def create
     @message = @group.messages.new(message_params)
@@ -27,3 +28,6 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
   end
 end
+=======
+end
+>>>>>>> parent of a676ac5... Add:　該当するアクションをコントローラに定義
