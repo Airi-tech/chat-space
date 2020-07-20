4e51b4ce012364007218e4af671b7276b6760bd0
Airi-tech <airis1631@gmail.com>
2020-07-20 19:28:14 +0900

    Add:　返ってきたJSONをdoneメソッドで受取り、HTMLを作成
    ・非同期通信の結果として返ってくるデータは、done(function(引数) { 処理 })の関数の引数で受け取る。この引数の値を元に、HTMLを組み立てる。
    ・HTMLを組み立てる部分については、buildHTMLというメソッドに指定。
    ・メッセージに画像が含まれているか否かで処理をif文で分岐。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="MessageBox">
+          <div class="MessageBox__MessageInfo">
+            <div class="MessageBox__MessageInfo__userName">
+              ${message.user_name}
+            </div>
+            <div class="MessageBox__MessageInfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="MessageBox__Message">
+            <p class="MessageBox__Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="MessageBox">
+        <div class="MessageBox__MessageInfo">
+          <div class="MessageBox__MessageInfo__userName">
+            ${message.user_name}
+          </div>
+          <div class="MessageBox__MessageInfo__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="MessageBox__Message">
+          <p class="MessageBox__Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+  
   $('.new_message').on('submit', function(e){
     e.preventDefault()
     let formData = new FormData(this);
@@ -10,4 +51,7 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
     })
