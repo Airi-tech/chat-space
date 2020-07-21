0df1f6dee9cc5790f099d0538b9d93ce9c55b17a
Airi-tech <airis1631@gmail.com>
2020-07-21 21:02:47 +0900

    Add:　自動更新が必要ない画面では行わないように設定
    ・autoReload.jsファイルを作成し、自動更新に必要な部分のみ記述した。
    これにより、自動更新が必要ない画面では自動更新を行わないように設定
    
    ・jQueryは今のところ全てのページにて発火するため、どの画面を見ていても自動更新処理が行わてしまう。このままでは、メッセージ更新を行わないページにおいてエラーが発生したり、無駄なトラフィックが発生してしまう。

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -1,3 +1,45 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="MessageBox" data-message-id=${message.id}>
+          <div class="MessageBox__MessageInfo">
+            <div class="MessageBox__MessageInfo__userName">
+              ${message.user_name}
+            </div>
+            <div class="MessageBox__MessageInfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="MessageBox__Message">
+            <p class="MessageBox__Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="MessageBox" data-message-id=${message.id}>
+        <div class="MessageBox__MessageInfo">
+          <div class="MessageBox__MessageInfo__userName">
+            ${message.user_name}
+          </div>
+          <div class="MessageBox__MessageInfo__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="MessageBox__Message">
+          <p class="MessageBox__Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
   let reloadMessages = function() {
     let last_message_id = $('.MessageBox:last').data("message-id") || 0;
     $.ajax({
@@ -21,3 +63,4 @@
     });
   };
   setInterval(reloadMessages, 7000);
+});
