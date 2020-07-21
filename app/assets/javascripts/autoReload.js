0743397ccadc749ea02976de19512599192edf87
Airi-tech <airis1631@gmail.com>
2020-07-21 21:01:22 +0900

    Add:　最新メッセージのidを取得できるよう実装
    ・作成したアクションを動かすリクエストを実装した。
    ・reloadMessagesという名前で関数を作成
    
    ・reloadMessages関数の中身
    1.カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
    2.ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
    3.ルーティングで設定した通りhttpメソッドをgetに指定
    4.dataオプションでリクエストに値を含める
    5.更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
    6.追加するHTMLの入れ物を作る
    7.配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
    8.メッセージが入ったHTMLに、入れ物ごと追加
    9.エラーが発生した際に対応するalertを設定

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -1,2 +1,23 @@
+  let reloadMessages = function() {
+    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        let insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.MessageList').append(insertHTML);
         $('.MessageList').animate({ scrollTop: $('.MessageList')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
   setInterval(reloadMessages, 7000);
